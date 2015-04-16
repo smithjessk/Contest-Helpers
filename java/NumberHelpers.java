fdf1f6f9f89de32be915b4a13696f4fc942f4b90
Jess Smith <smith.jessk@gmail.com>
2015-04-15 21:01:00 -0400

    Updated NumberHelpers.java for readability

--- java/NumberHelpers.java
+++ java/NumberHelpers.java
@@ -16,75 +16,44 @@ public class NumberHelpers {
 		return calcFibonacci(n - 1) + calcFibonacci(n - 2);
 	}
 
-	public static int tri(int m) {
-		return m * (m + 1) / 2;
+	public static int calcTriangle(int n) {
+		return n * (n + 1) / 2;
 	}
 
-
-	//=======Checking for specific number conditions========//
-
-	//Is it a prime number?
-	public static boolean isPrime(int a){
-		if(a%2 == 0 || a < 2){
-			return false;
+	public static boolean isPrime(int n) {
+		if (n == 2) {
+			return true; 
 		}
-		for(int i = 3; i <= Math.ceil(Math.sqrt(a)); i++){
-			if(a % i == 0){
-				return false;
+		for (int div = 2; div <= Math.sqrt(n); div++) {
+			if (n % div == 0) { 
+				return false; 
 			}
 		}
 		return true;
 	}
 
-	//Is it a Fibonacci number?
-	public static boolean isFib(int a){
+	public static boolean isFib(int a) {
 		return (isPerfectSquare((5*a*a) + 4) || isPerfectSquare( (5*a*a) - 4 ));
 	}
 
-	/*
-	* Needed this for the Fibonacci one, and just added it if useful elsewhere
-	*/
-	public static boolean isPerfectSquare(int a){
-		int root = (int) Math.sqrt(a);
-		return (a == root*root);
+	public static boolean isPerfectSquare(int n) {
+		int root = (int) Math.sqrt(n);
+		return (n == root * root);
 	}
 
-	//Is it a perfect number? (sum of divisors == number)
-	public static boolean isPerfectNum(int a){
+	// TODO: Confirm that this works
+	public static boolean isPerfectSquare(int n) {
+		return (Math.sqrt(n) % 1 == 0);
+	}
+
+	// For perfect numbers, Sum(divisors) = n
+	public static boolean isPerfectNum(int n) {
 		int sumDivs = 0;
-		for(int i = 1; i < a; i++){
-			if(a%i == 0)
-				sumDivs += i;
+		for (int i = 1; i < n; i++) {
+			if (n % i == 0) { 
+				sumDivs += i; 
+			}
 		}
-		return (sumDivs == a);
+		return (sumDivs == n);
 	}
-
-
-	//Idk if you want these in this file, or a separate one
-
-	/*
-	* I'm not 100% sure on this one, I may have missed a +1 somewhere or something.
-	*/
-	public static String caesarShift(String str, int shiftValue){
-		Str out = "";
-		for(int i = 0; i < str.length(); i++){
-			out += ((((str.toLowerCase().charAt(i) - 'a') + shiftValue) % 26) + 'a');
-		}
-		return out;
-	}
-
-	/*
-	*  I took a look at the stuff for changing bases and there's a way in the
-	*  Java API, using the Integer class:
-	*
-	*  To convert to another base:
-	*  Integer.toString(int num, int base);
-	*
-	*  To convert from a base to normal number:
-	*  Integer.parseInt(String number_representation, int base);
-	*  
-	*  So for example, to change from binary to hex:
-	*  int binary = 10;
-	*  String hexValue = Integer.toString(Integer.parseInt(binary, 2), 16);/
-	*/
 }
